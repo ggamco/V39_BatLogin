3786edd5db8a2bea1ee60771629f1f8166fef16c
cice <cice@aula8PovMacN08.local>
2017-01-27 22:06:06 +0100

    commit

--- App_BarLogin/BAT_LoginVC.swift
+++ App_BarLogin/BAT_LoginVC.swift
@@ -10,16 +10,109 @@ import UIKit
 
 class BAT_LoginVC: UIViewController {
 
+    //MARK: - Variables
+    var animacionChula : UIViewPropertyAnimator!
+    
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var myLogoBatman: UIImageView!
+    @IBOutlet weak var myTextoBatman: UIImageView!
+    @IBOutlet weak var myUsuarioLabel: UILabel!
+    @IBOutlet weak var myUsuarioTF: UITextField!
+    @IBOutlet weak var myPasswordLabel: UILabel!
+    @IBOutlet weak var myPasswordTF: UITextField!
+    @IBOutlet weak var myEntrarBTN: UIButton!
+    
+    
+    //MARK: - IBActions
+    @IBAction func saltaVC(_ sender: AnyObject) {
+        
+        let customBound = myEntrarBTN.bounds
+        UIView.animate(withDuration: 0.5,
+                       delay: 0.0,
+                       usingSpringWithDamping: 0.2,
+                       initialSpringVelocity: 10,
+                       options: .curveEaseInOut,
+                       animations: { 
+                        self.myEntrarBTN.bounds = CGRect(x: customBound.origin.x - 20,
+                                                         y: customBound.origin.y,
+                                                         width: customBound.width + 20,
+                                                         height: customBound.height)
+            }) { (void) in
+                let tabVC = self.storyboard?.instantiateViewController(withIdentifier: "RootTabBarVC") as! BAT_RootTabBarVC
+                tabVC.modalTransitionStyle = .crossDissolve
+                self.present(tabVC, animated: true, completion: nil)
+        }
+        
+    }
+    
+    //MARK: - LIFE VC
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        UIApplication.shared.statusBarStyle = .lightContent
 
+        myEntrarBTN.layer.cornerRadius = 5
+        objetosSinOpacidad()
+        objetosDesplazados()
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        iniciaAnimacion()
     }
+    
+    
+    func objetosSinOpacidad(){
+        
+        myLogoBatman.layer.opacity = 0
+        myTextoBatman.layer.opacity = 0
+        myUsuarioLabel.layer.opacity = 0
+        myUsuarioTF.layer.opacity = 0
+        myPasswordLabel.layer.opacity = 0
+        myPasswordTF.layer.opacity = 0
+        myEntrarBTN.layer.opacity = 0
+        
+    }
+    
+    func objetosDesplazados(){
+        
+        myLogoBatman.transform = CGAffineTransform(translationX: 0, y: 620)
+        myTextoBatman.transform = CGAffineTransform(translationX: 0, y: 620)
+        myUsuarioLabel.transform = CGAffineTransform(translationX: 0, y: 620)
+        myUsuarioTF.transform = CGAffineTransform(translationX: 0, y: 620)
+        myPasswordLabel.transform = CGAffineTransform(translationX: 0, y: 620)
+        myPasswordTF.transform = CGAffineTransform(translationX: 0, y: 620)
+        myEntrarBTN.transform = CGAffineTransform(translationX: 0, y: 620)
+        
+    }
+    
+    func iniciaAnimacion(){
+        
+        setAnimation(myDuracion: 0.6, myObjeto: myLogoBatman)
+        setAnimation(myDuracion: 0.7, myObjeto: myTextoBatman)
+        setAnimation(myDuracion: 0.8, myObjeto: myUsuarioLabel)
+        setAnimation(myDuracion: 0.9, myObjeto: myUsuarioTF)
+        setAnimation(myDuracion: 1.0, myObjeto: myPasswordLabel)
+        setAnimation(myDuracion: 1.1, myObjeto: myPasswordTF)
+        setAnimation(myDuracion: 1.2, myObjeto: myEntrarBTN)
+        
+    }
+    
+    func setAnimation(myDuracion: Double, myObjeto: UIView){
+        
+        animacionChula = UIViewPropertyAnimator(duration: myDuracion, curve: .easeInOut, animations: {
+            myObjeto.layer.opacity = 1
+            myObjeto.transform = CGAffineTransform.identity
+        })
+        
+        animacionChula.startAnimation()
+    }
+    
+
+    
+    
     
 
     /*
--- App_BarLogin/BAT_SplashVC.swift
+++ App_BarLogin/BAT_SplashVC.swift
@@ -10,16 +10,66 @@ import UIKit
 
 class BAT_SplashVC: UIViewController {
 
+    //MARK: - Variables LOCALES
+    var viewAnimator: UIViewPropertyAnimator!
+    var desbloqueoGesto = Timer()
+    
+    
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var myImagenLogoBatman: UIImageView!
+    @IBOutlet weak var myEtiquetaMarcaRegistradaLabel: UILabel!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //preparamos la animación
+        viewAnimator = UIViewPropertyAnimator(duration: 1.0,
+                                              curve: .easeInOut,
+                                              animations: nil)
+        
+        viewAnimator.addAnimations {
+            //Aqui hacemos que nuestra etiqueta se quede completemante transparente
+            self.myEtiquetaMarcaRegistradaLabel.layer.opacity = 0
+            self.myImagenLogoBatman.transform = CGAffineTransform(scaleX: 0.7, y: 0.7)
+            self.desbloqueoGesto = Timer.scheduledTimer(timeInterval: 1.5,
+                                                        target: self,
+                                                        selector: #selector(self.muestraAutomatico),
+                                                        userInfo: nil,
+                                                        repeats: false)
+        }
+        
+        viewAnimator.startAnimation()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func muestraAutomatico(){
+        //aqui creamos la otra animacion
+        let logoAnimation = UIViewPropertyAnimator(duration: 0.5, curve: .easeInOut) { 
+            self.myImagenLogoBatman.transform = CGAffineTransform(scaleX: 25, y: 25)
+        }
+        logoAnimation.startAnimation()
+        logoAnimation.addCompletion { (void) in
+            self.comienzoApp()
+        }
+    }
+    
+    func comienzoApp(){
+        let loginVC = storyboard?.instantiateViewController(withIdentifier: "LoginVC") as! BAT_LoginVC
+        loginVC.modalTransitionStyle = .crossDissolve
+        present(loginVC, animated: true, completion: nil)
+    }
+    
+    
     
 
     /*
